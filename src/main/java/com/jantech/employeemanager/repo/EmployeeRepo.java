88f705293ca05bb62ea839056751e6a814bd3c89
emmanuel-jan <emmanueljan80@gmail.com>
2022-06-29 16:02:38 +0300

    intial commit

---
+++ src/main/java/com/jantech/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,11 @@
+package com.jantech.employeemanager.repo;
+
+import com.jantech.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
